6b93c66055bb75fde9f09b792b4444fc835577c7
SifiFox <dima.antonovich.1999@gmail.com>
2024-07-13 00:28:33 +0300

    Updates

--- assets/favourites-page-DPasoDXR.js
+++
@@ -1,1 +0,0 @@
-import{j as s,H as r,c as e}from"./index-Dak0MoaV.js";const c=({title:a})=>s.jsxs(s.Fragment,{children:[s.jsx(r,{}),s.jsxs("div",{className:e.pageWrapper,children:[s.jsx("h1",{className:e.pageTitle,children:a}),s.jsx("div",{className:e.pageContent,children:s.jsx("span",{children:a})})]})]});export{c as default};
