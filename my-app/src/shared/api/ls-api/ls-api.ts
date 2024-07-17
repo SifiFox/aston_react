e2198fa6d2a9dbeb36eeb7ddf0948c15d097cb3e
SifiFox <dima.antonovich.1999@gmail.com>
2024-07-17 16:48:57 +0300

    fix: remove unused sources

--- my-app/src/shared/api/ls-api/ls-api.ts
+++ my-app/src/shared/api/ls-api/ls-api.ts
@@ -1,7 +1,4 @@
 import type { MovieBase } from "@/app/hooks/use-movies/types"
-import {signInWithPopup} from "firebase/auth";
-import {auth, provider} from "@/shared/config/firebase-config/firebase-config";
-
 export const checkAuth = callback => {
     const activeUser = JSON.parse(localStorage.getItem("activeUser"))
     callback(activeUser)
